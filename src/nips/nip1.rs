c2093c400c8e8b6e3c1cc6fff133f03dd552617c
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-12-04 22:33:50 +0100

    NIP 01 Async

--- src/nips/nip1.rs
+++ src/nips/nip1.rs
@@ -26,6 +26,7 @@ impl From<ClientError> for NIP1Error {
 }
 
 impl Client {
+    #[cfg(not(feature = "async"))]
     /// Set the metadata of the identity
     /// # Example
     /// # Example
@@ -77,6 +78,62 @@ impl Client {
         Ok(event)
     }
 
+    #[cfg(feature = "async")]
+    /// Set the metadata of the identity asynchronously
+    /// # Example
+    /// ```rust
+    /// use nostr_rust::{nostr_client::Client, Identity};
+    /// use std::str::FromStr;
+    /// use async_std::task;
+    ///
+    /// let mut client = Client::new(vec![env!("RELAY_URL")]).unwrap();
+    /// let identity = Identity::from_str(env!("SECRET_KEY")).unwrap();
+    ///
+    /// task::spawn(async move {
+    ///     // Here we set the metadata of the identity asynchronously
+    ///     client.set_metadata(&identity, Some("Rust Nostr Client"), Some("Automated account for Rust Nostr Client tests :)"), None, 0).await.unwrap();
+    /// });
+    /// ```
+    pub async fn set_metadata(
+        &mut self,
+        identity: &Identity,
+        name: Option<&str>,
+        about: Option<&str>,
+        picture: Option<&str>,
+        difficulty_target: u16,
+    ) -> Result<Event, NIP1Error> {
+        let mut json_body = json!({});
+
+        if name.is_none() && about.is_none() && picture.is_none() {
+            return Err(NIP1Error::NoMetadata);
+        }
+
+        if let Some(name) = name {
+            json_body["name"] = json!(name);
+        }
+
+        if let Some(about) = about {
+            json_body["about"] = json!(about);
+        }
+
+        if let Some(picture) = picture {
+            json_body["picture"] = json!(picture);
+        }
+
+        let event = EventPrepare {
+            pub_key: identity.public_key_str.clone(),
+            created_at: get_timestamp(),
+            kind: 0,
+            tags: vec![],
+            content: json_body.to_string(),
+        }
+        .to_event(identity, difficulty_target);
+
+        self.publish_event(&event).await?;
+        Ok(event)
+    }
+
+    #[cfg(not(feature = "async"))]
     /// Publish a text note (text_note) event
     /// # Example
     /// ```rust
@@ -107,6 +164,44 @@ impl Client {
         Ok(event)
     }
 
+    #[cfg(feature = "async")]
+    /// Publish a text note (text_note) event asynchronously
+    /// # Example
+    /// ```rust
+    /// use nostr_rust::{nostr_client::Client, Identity, utils::get_timestamp};
+    /// use std::str::FromStr;
+    /// use async_std::task;
+    ///
+    /// let mut client = Client::new(vec![env!("RELAY_URL")]).unwrap();
+    /// let identity = Identity::from_str(env!("SECRET_KEY")).unwrap();
+    /// let message = format!("Hello Nostr! {}", get_timestamp());
+    ///
+    /// task::spawn(async move {
+    ///     // Here we publish the text note asynchronously
+    ///     client.publish_text_note(&identity, &message, &vec![], 0).await.unwrap();
+    /// });
+    /// ```
+    pub async fn publish_text_note(
+        &mut self,
+        identity: &Identity,
+        content: &str,
+        tags: &[Vec<String>],
+        difficulty_target: u16,
+    ) -> Result<Event, NIP1Error> {
+        let event = EventPrepare {
+            pub_key: identity.public_key_str.clone(),
+            created_at: get_timestamp(),
+            kind: 1,
+            tags: tags.to_vec(),
+            content: content.to_string(),
+        }
+        .to_event(identity, difficulty_target);
+
+        self.publish_event(&event).await?;
+        Ok(event)
+    }
+
+    #[cfg(not(feature = "async"))]
     /// Add recommended relay server
     /// # Example
     /// ```rust
@@ -134,6 +229,41 @@ impl Client {
         .to_event(identity, difficulty_target);
 
         self.publish_event(&event)?;
+        Ok(event)
+    }
+
+    #[cfg(feature = "async")]
+    /// Add recommended relay server asynchronously
+    /// # Example
+    /// ```rust
+    /// use nostr_rust::{nostr_client::Client, Identity};
+    /// use std::str::FromStr;
+    /// use async_std::task;
+    ///
+    /// let mut client = Client::new(vec![env!("RELAY_URL")]).unwrap();
+    /// let identity = Identity::from_str(env!("SECRET_KEY")).unwrap();
+    ///
+    /// task::spawn(async move {
+    ///     // Here we add the recommended relay server asynchronously
+    ///     client.add_recommended_relay(&identity, "wss://relay.damus.io", 0).await.unwrap();
+    /// });
+    /// ```
+    pub async fn add_recommended_relay(
+        &mut self,
+        identity: &Identity,
+        relay: &str,
+        difficulty_target: u16,
+    ) -> Result<Event, NIP1Error> {
+        let event = EventPrepare {
+            pub_key: identity.public_key_str.clone(),
+            created_at: get_timestamp(),
+            kind: 2,
+            tags: vec![],
+            content: relay.to_string(),
+        }
+        .to_event(identity, difficulty_target);
+
+        self.publish_event(&event).await?;
         Ok(event)
     }
 }
